108f9fd7fbef09cafedfb96e1ba58b0138c20740
Angelica Fallas Blanco <angelica.fallas@gmail.com>
2023-12-14 15:09:21 -0600

    add mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Ange9/puppy
 
 go 1.21.5
+
+require github.com/Ange9/dog v0.0.0-20231214210440-eaa604ffbc23 // indirect
--- puppy.go
+++ puppy.go
@@ -1,8 +1,20 @@
 package puppy
 
+import (
+	"github.com/Ange9/dog"
+)
+
 func Bark() string {
 	return "Boof!"
 }
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.BigBark(Bark())
+}
+
+func BigBarks() string {
+	return dog.BigBarks(Barks())
 }
